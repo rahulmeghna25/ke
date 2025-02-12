216f392fda0a40515ddbd779a1c1d21044613add
rahulmeghna25 <coctopper.10@gmail.com>
2025-02-12 17:45:36 +0530

    Update rahul.py

--- rahul.py
+++ rahul.py
@@ -144,7 +144,7 @@ async def attack(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     # Execute attack command
     try:
         process = subprocess.Popen(
-            f"./Rahul {ip} {port} {duration} 700",  # Replace with actual attack tool command
+            f"./raja {ip} {port} {duration} 700",  # Replace with actual attack tool command
             shell=True,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE
